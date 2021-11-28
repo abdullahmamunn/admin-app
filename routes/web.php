185f61eb4c370e4e7e15a9fa0563231f56f8edb0
abdullahmamunn <almamun00021@gmail.com>
2021-11-28 10:45:19 -0800

    test

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data['mamun'] = "mamun";
+
+    return view('welcome-mamun',$data);
 });
